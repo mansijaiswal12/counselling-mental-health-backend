2f20512013c9880b08c66508ad6fdc90e5bd8eb5
mansijaiswal <96405456+mansijaiswal12@users.noreply.github.com>
2024-03-30 16:51:44 +0530

    initial setup of project

---
+++ index.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const ContactUsmodel = require("./model/contactForm");
+const FeedbackModel = require("./model/feedbackForm");
+
+const server = express();
+const port = process.env.PORT || 8080;
+const mongodbUri = process.env.MONGODB_URI;
+
+server.use(express.json());
+server.use(cors());
+
+mongoose.connect(mongodbUri);
+
+server.get("/", (req, res) => {
+  res.send("Home route");
+});
+
+// contactus form route
+server.post("/contactus", async (req, res) => {
+  try {
+    const { name, mobileNo, email, date, time } = req.body;
+    console.log(name, mobileNo, email, date, time);
+
+    const contactdata = await ContactUsmodel.create({
+      name,
+      mobileNo,
+      email,
+      date,
+      time,
+    });
+
+    res.status(201).json(contactdata);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Internal server Error");
+  }
+});
+
+// feedback form route
+server.post("/feedback", async (req, res) => {
+  try {
+    const { name, comments, rating, therapyNames } = req.body;
+    console.log(name, comments, rating, therapyNames);
+
+    const feedbackData = await FeedbackModel.create({
+      name,
+      comments,
+      rating,
+      therapyNames,
+    });
+
+    res.status(201).json(feedbackData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Internal server Error");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`server running on port http://localhost:${port}`);
+});
+
+
+
